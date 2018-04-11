0f5bf0cb76b4e4a41a9a5128866959dd1b3004f9
Ding Yaoyao <idy002@163.com>
2018-04-11 21:29:03 +0800

    add "@/dev/stdin"

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -classpath "./lib/antlr-4.7.1-complete.jar"
+find ./src -name *.java | javac -d bin -classpath "./lib/antlr-4.7.1-complete.jar" @/dev/stdin
